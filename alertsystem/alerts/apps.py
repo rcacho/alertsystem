33f63f21fa43ee98072e4d65941973aa1b8fd119
rcacho <rcacho@hotmail.ca>
2020-12-06 20:25:12 -0800

    added signal to ready

--- alertsystem/alerts/apps.py
+++ alertsystem/alerts/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class AlertsConfig(AppConfig):
     name = 'alerts'
+
+    def ready(self):
+        from alerts.models import GlucosePoint
+        from alerts.signals import save_glucose_point
+        post_save.connect(save_glucose_point, sender=GlucosePoint)
